7b350e936a13a275dbadab57f782978a27c04910
maitreebain <maitreebain@gmail.com>
2019-12-16 20:31:37 -0500

    Initial Commit

---
+++ PodcastFavorites/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodcastFavorites
+//
+//  Created by Maitree Bain on 12/16/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
